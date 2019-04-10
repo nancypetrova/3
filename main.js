96b21bfe1b6433d946a9c4c52f030923d5c06470
nancypetrova <nencypetrova@mail.ru>
2019-04-10 13:02:19 +0300

    new

---
+++ main.js
@@ -0,0 +1,6 @@
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+document.body.appendChild(canvas); // adds the canvas to the body element
+canvas = document.createElement('canvas');
+canvas.width = 320;
+canvas.height = 480;
